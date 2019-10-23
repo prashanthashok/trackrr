6d3ccc4beea2a1d5d2b065bd903e10bb8683e0f5
prashanthashok <prashanth.ashokram@gmail.com>
2019-10-22 23:03:47 -0400

    Finished v1 of header with brand and collapsible menu

---
+++ web-app/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [AppComponent, LandingPageComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
